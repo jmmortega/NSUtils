369d70f0ccc94a7eae47f969583bcc18527ab247
jmmontero <jmmontero.ortega@gmail.com>
2016-07-15 08:44:18 +0200

    Added new project to utils for MvvmCross

---
+++ NSUtilsMvx/ExtensionMethods/ExtensionMethodsMvx.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Windows.Input;
+
+namespace NSUtilsMvx.ExtensionMethods
+{
+    public static class ExtensionMethodsMvx
+    {
+        public static void Execute(this ICommand command)
+        {
+            command.Execute(null);
+        }
+    }
+}
--- NSUtilsMvx/Properties/AssemblyInfo.cs
+++ NSUtilsMvx/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("NSUtils")]
+[assembly: AssemblyTitle("NSUtilsMvx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NSUtils")]
+[assembly: AssemblyProduct("NSUtilsMvx")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
